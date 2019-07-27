4e1ebb887b9ec547d06803b5ff2de29086fd86dd
saeedbah <47155904+saeedbah@users.noreply.github.com>
2019-07-27 15:48:37 +0430

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php
 $dburl ="besoyezohor.cf";
-$dbuser ="besoyezo_saeed";
+$dbuser ="besoyezo_1";
 $dbpass ="SAba1380831";
-$dbname ="besoyezo_1";
+$dbname ="besoyezo_saeed";
 $bot_token = '692870367:AAGxDzx5OJHr63aP6_TxLY7lgvZ66jWjfCY';
 $conn = "mysql:host=$dburl;dbname=$dbname";
 $con = new PDO($conn, $dbuser, $dbpass);
